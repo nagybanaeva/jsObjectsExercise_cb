9860fef783c1bea07c291b678f3c5aaca28e72e3
Nagy-Bana Éva <nagybanaeva@gmail.com>
2020-06-18 23:19:07 +0200

    add object method

--- script.js
+++ script.js
@@ -10,7 +10,10 @@ var myBook = {
     'p124': 'I wanted you to see something about her—I wanted you to see what real courage is, instead of getting the idea that courage is a man with a gun in his hand. It\'s when you know you\'re licked before you begin but you begin anyway and you see it through no matter what. You rarely win, but sometimes you do.',
     'p309': '"An\' they chased him \'n\' never could catch him \'cause they didn\'t know what he looked like, an\' Atticus, when they finally saw him, why he hadn\'t done any of those things... Atticus, he was real nice..." His hands were under my chin, pulling up the cover, tucking it around me. "Most people are, Scout, when you finally see them."'
   },
-'borrowed by':'undifined'
+'borrowed by':'undifined',
+'page marker': function () {
+	prompt('Melyik oldalon tartasz?');
+}
 };
 
 document.write (myBook);
